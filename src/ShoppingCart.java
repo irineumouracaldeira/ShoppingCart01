955c3e288b418fde5fd47a93d38a032252ef4c2c
Aluno <Aluno@10.85.194.76>
2024-06-07 13:21:51 -0300

    Commit project ShoppingCart01

---
+++ src/ShoppingCart.java
@@ -0,0 +1,19 @@
+
+public class ShoppingCart {
+    public static void main (String[] args){
+        String custName = "Alex";
+        String itemDesc = "Camisa";
+        String message = custName + " Quer comprar uma" + itemDesc;
+       
+        System.out.println(message);
+        
+        // Assign the message variable
+
+
+        // Print and run the code
+
+
+    }
+
+    
+}
